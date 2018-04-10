756686a46afa771449e4c1c4cf2ecbc90f051643
dean joseph <deanjoseph3@hotmail.com>
2018-04-10 10:53:36 +0000

    commited new files

---
+++ JS/js.js
@@ -0,0 +1,11 @@
+/*
+*
+ * Created by avi on 10/10/2016.
+js.js
+
+
+function showPic1(){
+    document.getElementsByClassName("bigPics")
+        .style.backgroundImage = 'url(Images/anfield_stand1.jpg")';
+
+}//showPic1*/
